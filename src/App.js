d8815a3df2ddcf13765e2e336c8bf87bd54ed65b
Rabbani <rabbanisk162002@gmail.com>
2025-08-24 22:27:51 +0530

    changed some colors in navbar

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
       {/* <Footer /> */}
       <Routes>
         <Route path="/home" element={<Home />} />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,18 +3,17 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div className="main">
-            <nav className="navbar navbar-expand-lg bg-body-tertiary" id="navbar">
+            <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-
                     <Link to="/home" className="navbar-brand">RABBANI</Link>
                     
                     <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <Link to="/home" className="nav-link active" aria-current="page">Home</Link>
+                                <Link to="/home" className="nav-link" aria-current="page">Home</Link>
                             </li>
                             <li className="nav-item">
                                 <Link to="/about" className="nav-link">About Me</Link>
